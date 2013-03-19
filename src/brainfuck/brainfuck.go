c1f4823dd2aa1284f51bb4cda8bc543dca07272f
Tanner Smith <tanner@tssoftware.net>
2013-03-18 22:15:55 -0400

    Implement run.

--- src/brainfuck/brainfuck.go
+++ src/brainfuck/brainfuck.go
@@ -3,11 +3,79 @@ package brainfuck
 import (
   "bytes"
   "errors"
+  "fmt"
 )
 
 func Run(code string) error {
   if Validate(code) == false {
     return errors.New("Code was not valid brainfuck.")
+  }
+
+  array := make([]byte, 30000, 30000)
+
+  index := 0
+
+  loop := 0
+
+  i := 0
+
+  for i < len(code) {
+    v := code[i]
+
+    i++
+
+    switch v {
+    case '>':
+      index++
+    case '<':
+      index--
+    case '+':
+      array[index]++
+    case '-':
+      array[index]--
+    case '.':
+      fmt.Printf("%c", array[index])
+    case ',':
+      var input string;
+      n, err := fmt.Scan(input)
+
+      if err != nil && n >= 1 {
+        array[index] = input[0]
+      }
+    case '[':
+      if array[index] == 0 {
+        loop = 1
+
+        for loop > 0 {
+          i++
+
+          value := code[i]
+
+          if value == '[' {
+            loop++
+          } else if value == ']' {
+            loop--
+          }
+        }
+
+        i++
+      }
+    case ']':
+      loop = 1
+      i--
+
+      for loop > 0 {
+        i--
+
+        value := code[i]
+
+        if value == '[' {
+          loop--
+        } else if value == ']' {
+          loop++
+        }
+      }
+    }
   }
 
   return nil
