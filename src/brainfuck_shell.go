08f890dc9d93cd697dac43bf4bf9a52997fd57e7
Tanner Smith <tanner@tssoftware.net>
2013-03-18 22:52:23 -0400

    Add CLI interface to brainfuck.

--- src/brainfuck_shell.go
+++ src/brainfuck_shell.go
@@ -3,10 +3,33 @@ package main
 import (
   "fmt"
   "brainfuck"
+  "flag"
+  "io/ioutil"
 )
 
 func main() {
-  err := brainfuck.Run("++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.")
+  var path = flag.String("filename", "brainfuck.bf", "Name of file to run")
+  var validate = flag.Bool("validate", false, "Validates instead of running")
 
-  fmt.Println("\n", err)
+  flag.Parse();
+
+  content, err := ioutil.ReadFile(*path)
+
+  if err != nil {
+    fmt.Println(err)
+
+    return
+  }
+
+  code := string(content)
+
+  if (*validate) {
+    if brainfuck.Validate(code) {
+      fmt.Println("Valid code.")
+    } else {
+      fmt.Println("Non-valid code.")
+    }
+  } else {
+    brainfuck.Run(code)
+  }
 }
